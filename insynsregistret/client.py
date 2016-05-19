f10e3fd08ab9c10db440d399f07ea7b8422626cd
Daniel Jonsson <wd.jonsson@gmail.com>
2016-05-19 14:32:41 +0200

    Refactored the client to support all endpoints

--- insynsregistret/client.py
+++ insynsregistret/client.py
@@ -9,86 +9,85 @@ import xml.etree.ElementTree as ET
 
 
 class Session(object):
-    """
-    Sets up a session object that is used to interact with insynssok. In order to produce valid requests
-    we need a session cookie (which is automatically registered by using requests.Session() in the __init__ method
-    and using it to perform the __event_validation__ method.
-
-    The cookie that is required for the api is acquired by simply performing a GET request to SearchPage.apsx.
-
-    'cookie': ASP.NET_SessionId=2hp0ly45dz2ceh45bbia2ufw;
-              WT_FPC=id=f9b0b3db-5600-4c8c-a920-48cace6b052dlv=1463047439657ss=1463045836001;
-              insynsok=ffffffffc3a01c7b45525d5f4f58455e445a4a423660
-
-    The cookie is stored in the Session() object and applied to all requests. In order to perform valid requests to
-    insynssok we also require the asp.net parameters (__EVENTVALIDATION and __VIEWSTATE).
-
-    These parameters are stored in hidden input fields as values on the page. Why are they there?
-    The short answer is event validation is designed to protect the web site from having values injected into the page
-    that can be used to exploit the application in some way.
-
-    You can read more about it here:
-    https://msdn.microsoft.com/en-us/library/system.web.ui.page.enableeventvalidation.aspx
-
-    We add these parameters by calling the private method __event_validation__, which parses the html, finds the fields
-    and store them in ASP_NET_FIELDS(dict).
-
-    """
 
     DEFAULT_SETTINGS = {
-        'url': 'http://insynsok.fi.se/SearchPage.aspx',
         'cache_directory': '~/.cache-insyn/'
     }
 
-    ASP_NET_FIELDS = {}
-
     def __init__(self):
-        self.session = requests.Session()
-        self.__event_validation__()
-        self.cache = Cache(self.DEFAULT_SETTINGS)
-        self.cache_dir = self.cache.get_cache_directory()
+        self.__session = requests.Session()
+        self.__cache = Cache(self.DEFAULT_SETTINGS)
+        self.__cache_dir = self.__cache.get_cache_directory()
 
-    def __event_validation__(self):
-        get_response = self.session.get(self.DEFAULT_SETTINGS['url'])
-        soup = BeautifulSoup(get_response.content, 'lxml')
-        e = self.ASP_NET_FIELDS['__EVENTVALIDATION'] = soup.find("input", {"id": "__EVENTVALIDATION"}).get('value')
-        v = self.ASP_NET_FIELDS['__VIEWSTATE'] = soup.find("input", {"id": "__VIEWSTATE"}).get('value')
-        return e, v
+    def search_transactions(self, from_date, to_date):
+        filename = self.__cache_dir + ('_Transaktioner_%s_%s__(sv-SE).xml' % (from_date, to_date))
+        url = 'http://insynsok.fi.se/SearchPage.aspx'
+        e, v = self.__get_request_to_setup_asp_fields(url)
 
-    def transactions(self, from_date, to_date):
-        filename = self.cache_dir + ('_Transaktioner_%s_%s__(sv-SE).xml' % (from_date, to_date))
-        if not self.cache.file_exist(filename):
-            self.get(payload=self.__transactions_payload__(from_date, to_date))
-
-        if self.cache.is_empty(filename):
+        if not self.__cache.file_exist(filename):
+            response = self.__session.post(url, data=self.__request_payload(e, v, drop_down='Transaktioner', from_date=from_date, to_date=to_date))
+            z = zipfile.ZipFile(StringIO.StringIO(response.content))
+            z.extractall(path=self.__cache_dir)
+        if self.__cache.is_empty(filename):
             return None
-
         return ET.parse(filename).getroot()
 
-    def insiders(self):
-        filename = self.cache_dir + '_Insyn__(sv-SE).xml'
-        self.get(payload=self.__insiders_payload__())
+    def search_insiders(self):
+        filename = self.__cache_dir + '_Insyn__(sv-SE).xml'
+        url = 'http://insynsok.fi.se/SearchPage.aspx'
+        e, v = self.__get_request_to_setup_asp_fields(url)
+        response = self.__session.post(url, data=self.__request_payload(e, v, drop_down='Insyn', from_date='2015-11-18', to_date='2015-12-18'))
+
+        z = zipfile.ZipFile(StringIO.StringIO(response.content))
+        z.extractall(path=self.__cache_dir)
         return ET.parse(filename).getroot()
 
-    def get(self, payload=None):
-        z = self.download_zip_file(payload)
-        z.extractall(path=self.cache_dir)
-        return z
+    def search_company(self, company_name=None, org_number=None):
+        url = 'http://insynsok.fi.se/Startpage.aspx?searchtype=0&culture=sv-SE'
+        response = self.__session.post(url, data=self.__search_payload(url, company_name=company_name, org_number=org_number))
+        e, v = self.__get_asp_fields_from_response(response)
 
-    def download_zip_file(self, payload):
-        response = self.session.post(self.DEFAULT_SETTINGS['url'], data=payload)
-        print'----'
-        print response.url
-        print payload
-        print '----'
-        return zipfile.ZipFile(StringIO.StringIO(response.content))
+        if company_name is 'co':
+            print response.content
 
-    def __transactions_payload__(self, from_date, to_date):
+        response = self.__session.post(response.url, data=self.__request_payload(e, v, drop_down='Transaktioner', from_date='2015-11-18', to_date='2015-12-18'))
+        z = zipfile.ZipFile(StringIO.StringIO(response.content))
+        z.extractall(path=self.__cache_dir)
+
+        response2 = self.__session.post(response.url, data=self.__request_payload(e, v, drop_down='Innehav', from_date='2015-11-18', to_date='2015-12-18'))
+        z = zipfile.ZipFile(StringIO.StringIO(response2.content))
+        z.extractall(path=self.__cache_dir)
+
+        response2 = self.__session.post(response.url, data=self.__request_payload(e, v, drop_down='HistorisktInnehav', from_date='2016-01-01', to_date='2016-05-30'))
+        z = zipfile.ZipFile(StringIO.StringIO(response2.content))
+        z.extractall(path=self.__cache_dir)
+
+        response2 = self.__session.post(response.url, data=self.__request_payload(e, v, drop_down='Insyn', from_date='2015-11-18', to_date='2015-12-18'))
+        z = zipfile.ZipFile(StringIO.StringIO(response2.content))
+        z.extractall(path=self.__cache_dir)
+
+        response2 = self.__session.post(response.url, data=self.__request_payload(e, v, drop_down='Befattningsforandringar', from_date='2015-11-18', to_date='2015-12-18'))
+        z = zipfile.ZipFile(StringIO.StringIO(response2.content))
+        z.extractall(path=self.__cache_dir)
+
+    def __get_request_to_setup_asp_fields(self, url):
+        response = self.__session.get(url)
+        return self.__get_asp_fields_from_response(response)
+
+    @staticmethod
+    def __get_asp_fields_from_response(response):
+        soup = BeautifulSoup(response.content, 'lxml')
+        event_validation = soup.find("input", {"id": "__EVENTVALIDATION"}).get('value')
+        view_state = soup.find("input", {"id": "__VIEWSTATE"}).get('value')
+        return event_validation, view_state
+
+    @staticmethod
+    def __request_payload(e, v, drop_down, from_date, to_date):
         return {
-            '__EVENTVALIDATION':            self.ASP_NET_FIELDS['__EVENTVALIDATION'],
-            '__VIEWSTATE':                  self.ASP_NET_FIELDS['__VIEWSTATE'],
+            '__EVENTVALIDATION':            e,
+            '__VIEWSTATE':                  v,
             '__VIEWSTATEGENERATOR':         '53A94410',
-            'ctl00$main$DropDownList1':     'Transaktioner',
+            'ctl00$main$DropDownList1':     drop_down,
             'ctl00$main$ImageButton1.x':    '14',
             'ctl00$main$ImageButton1.y':    '14',
             'ctl00$main$ResultFormatGroup': 'optExport',
@@ -96,14 +95,15 @@ class Session(object):
             'ctl00$main$tomDate':           to_date
         }
 
-    def __insiders_payload__(self):
+    def __search_payload(self, url, company_name=None, org_number=None):
+        e, v = self.__get_request_to_setup_asp_fields(url)
         return {
-            '__EVENTVALIDATION':            self.ASP_NET_FIELDS['__EVENTVALIDATION'],
-            '__VIEWSTATE':                  self.ASP_NET_FIELDS['__VIEWSTATE'],
-            '__VIEWSTATEGENERATOR':         '53A94410',
-            'ctl00$main$DropDownList1':     'Insyn',
-            'ctl00$main$ImageButton1.x':    '23',
-            'ctl00$main$ImageButton1.y':    '16',
-            'ctl00$main$ResultFormatGroup': 'optExport',
+            '__EVENTVALIDATION':            e,
+            '__VIEWSTATE':                  v,
+            '__VIEWSTATEGENERATOR':         'C8125695',
+            'ctl00$main$txtNamn':           company_name,
+            'ctl00$main$txtOrgNr':          org_number,
+            'ctl00$main$btnSeekkBolag.x':   '18',
+            'ctl00$main$btnSeekkBolag.y':   '5',
         }
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -9,7 +9,7 @@ from nose.tools import assert_is_none, assert_is_not_none, assert_raises
 
 
 def test_valid_date_should_not_return_none():
-    date_xml = Session().transactions(from_date='2015-11-18', to_date='2015-12-18')
+    date_xml = Session().search_transactions(from_date='2015-11-18', to_date='2015-12-18')
     for i, table in enumerate(date_xml):
         for data in table:
             print data.tag, data.text
@@ -19,7 +19,7 @@ def test_valid_date_should_not_return_none():
 
 
 def test_invalid_date_in_the_1900s_should_return_none():
-    assert_is_none(Session().transactions(from_date='1901-05-12', to_date='1901-05-15'))
+    assert_is_none(Session().search_transactions(from_date='1901-05-12', to_date='1901-05-15'))
 
 
 def test_list_cache_files():
@@ -33,5 +33,23 @@ def test_making_invalid_dir():
     with assert_raises(OSError):
         Cache().mkdir_p('/')
 
+
 def test_get_insynpersoner():
-    Session().insiders()
+    Session().search_insiders()
+
+
+def test_company_search_by_name():
+    Session().search_company(company_name='tobii')
+
+
+def test_company_search_by_org_no():
+    Session().search_company(org_number='556012-5790')
+
+
+def test_get_several_search_results():
+    Session().search_company(company_name='co')
+
+
+def test_get_no_companies():
+    Session().search_company(company_name='this-search-will-not-return-any-hits')
+
